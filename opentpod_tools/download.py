ae6528c82f424935286b568a7e04af15d84a65fc
Jan Harkes <jaharkes@cs.cmu.edu>
2020-11-03 08:46:49 -0500

    Add LICENSE file.
    Individual files were already tagged with license tags, but the
    copyright notifications and top-level LICENSE file were still missing.

--- opentpod_tools/download.py
+++ opentpod_tools/download.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 #
+#  Copyright (c) 2020 Carnegie Mellon University
+#  All rights reserved.
+#
 # SPDX-License-Identifier: Apache-2.0
 #
 """Download datasets from one or more CVAT tasks.
